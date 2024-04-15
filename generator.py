88338d9bf6bb5f2245d4469acb9f3e21e83bd8bc
eugenetsi <evgenios.tsigkanos@gmail.com>
2024-04-15 12:45:18 +0300

    adding relative paths

--- generator.py
+++ generator.py
@@ -1,17 +1,26 @@
+import os
 import random
 import pickle
 
-DATAPATH = "./data/"
+# Get the directory of the current script
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATAPATH = os.path.join(BASE_DIR, "data/")
 NOUNS = "nouns/"
 ADJ = "adjectives/"
 
 def get_name(sep='_'):
-    with open(DATAPATH + NOUNS + 'nouns.pkl', 'rb') as f:
+    nouns_path = os.path.join(DATAPATH, NOUNS, 'nouns.pkl')
+    adjectives_path = os.path.join(DATAPATH, ADJ, 'adjectives.pkl')
+
+    with open(nouns_path, 'rb') as f:
         nouns = pickle.load(f)
-    with open(DATAPATH + ADJ + 'adjectives.pkl', 'rb') as f:
+    with open(adjectives_path, 'rb') as f:
         adjectives = pickle.load(f)
+
     if sep not in ['_', '-', '', '.']:
         raise Exception('Not valid separator')
+
     return f'{random.choice(adjectives)}{sep}{random.choice(nouns)}'
 
 if __name__ == "__main__":
